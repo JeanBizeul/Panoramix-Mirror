3ca5a7730c6b296f5455cbe8844e34bf74bc900d
JeanBizeul <jean.bizeul@epitech.eu>
2025-05-07 06:07:20 +0200

    ✨ Refactor create_villagers function and implement launch_panoramix for threading

--- src/panoramix.c
+++ src/panoramix.c
@@ -17,7 +17,6 @@ villager_t **create_villagers(unsigned int count, unsigned int nb_fights,
         return NULL;
     for (unsigned int i = 0; i < count; i++) {
         villagers_list[i] = malloc(sizeof(villager_t));
-
         if (villagers_list[i] == NULL) {
             free(villagers_list);
             return NULL;
@@ -25,7 +24,7 @@ villager_t **create_villagers(unsigned int count, unsigned int nb_fights,
         villagers_list[i]->id = i;
         villagers_list[i]->nb_fights = nb_fights;
         villagers_list[i]->pot = pot;
-    };
+    }
     villagers_list[count] = NULL;
     return villagers_list;
 }
@@ -48,5 +47,24 @@ druid_t *create_druid(unsigned int nb_refills, pot_t *pot)
         return NULL;
     druid->nb_refills = nb_refills;
     druid->pot = pot;
+    if (sem_init(&druid->Druid_ready_sem, 0, 0) == -1) {
+        free(druid);
+        return NULL;
+    }
     return druid;
+}
+
+void launch_panoramix(villager_t **villagers, druid_t *druid)
+{
+    pthread_t villager_threads[VILLAGERS];
+    pthread_t druid_thread_id;
+
+    pthread_create(&druid_thread_id, NULL, druid_thread, druid);
+    sem_wait(&druid->Druid_ready_sem);
+    for (unsigned int i = 0; i < VILLAGERS; i++)
+        pthread_create(&villager_threads[i], NULL, village_thread,
+            villagers[i]);
+    for (unsigned int i = 0; i < VILLAGERS; i++)
+        pthread_join(villager_threads[i], NULL);
+    pthread_join(druid_thread_id, NULL);
 }
