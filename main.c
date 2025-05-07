868648a9a5ec134f9a6d6c41e43409770b2389c5
JeanBizeul <jean.bizeul@epitech.eu>
2025-05-07 06:08:31 +0200

    ✨ Refactor main function to initialize pot, druid, and villagers and launching the program

--- main.c
+++ main.c
@@ -6,9 +6,22 @@
 */
 
 #include <stdio.h>
+#include "panoramix.h"
 
 int main(void)
 {
-    printf("Hello, Panoramix!\n");
+    pot_t *pot = create_pot();
+    druid_t *druid = create_druid(REFILLS, pot);
+    villager_t **villagers = create_villagers(VILLAGERS, NB_FIGHTS, pot);
+
+    if (pot == NULL || druid == NULL || villagers == NULL) {
+        fprintf(stderr, "Error: Memory allocation failed.\n");
+        return 84;
+    }
+    if (!init_shared()) {
+        fprintf(stderr, "Error: Initialization failed.\n");
+        return 84;
+    }
+    launch_panoramix(villagers, druid);
     return 0;
 }
