0da8287817ce7cecdb77979b6aa6109b628d814f
JeanBizeul <jean.bizeul@epitech.eu>
2025-05-07 06:06:26 +0200

    ✨ Initialize semaphores and mutexes in shared.c

--- src/shared.c
+++ src/shared.c
@@ -13,3 +13,31 @@ pthread_mutex_t Pot_mutex;
 pthread_mutex_t Refills_left_mutex;
 sem_t Potions_sem;
 sem_t Call_druid_sem;
+
+bool init_semaphores(void)
+{
+    if (sem_init(&Potions_sem, 0, 0) == -1)
+        return false;
+    if (sem_init(&Call_druid_sem, 0, 0) == -1)
+        return false;
+    return true;
+}
+
+bool init_mutexes(void)
+{
+    if (pthread_mutex_init(&Pot_mutex, NULL) != 0)
+        return false;
+    if (pthread_mutex_init(&Refills_left_mutex, NULL) != 0)
+        return false;
+    return true;
+}
+
+bool init_shared(void)
+{
+    Refills_left = REFILLS;
+    if (!init_semaphores())
+        return false;
+    if (!init_mutexes())
+        return false;
+    return true;
+}
